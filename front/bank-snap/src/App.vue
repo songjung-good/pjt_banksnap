08f3515f9d69b21c2f2bcf1db3df16ce35f5bf87
YH <139380475+songjung-good@users.noreply.github.com>
2023-11-14 15:38:23 +0900

    네비게이션바

--- front/bank-snap/src/App.vue
+++ front/bank-snap/src/App.vue
@@ -1,11 +1,22 @@
 <template>
+  <header>
+    <img alt="Pjt logo" class="logo" src="@/assets/exlogo.gif" width="125" height="125" />
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'main' }">Home</RouterLink>
+        <RouterLink :to="{ name: 'interestrate' }">금리비교</RouterLink>
+        <RouterLink :to="{ name: 'exchangecalculator' }">환율계산기</RouterLink>
+        <RouterLink :to="{ name: 'community' }">커뮤니티</RouterLink>
+        <RouterLink :to="{ name: 'map' }">MAP</RouterLink>
+        <!-- <RouterLink :to="{ name: 'user', params: {id: userId} } ">User</RouterLink> -->
+        <!-- <RouterLink :to="{ name: 'login' } ">Login</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+
   <div>
     <h1>MAIN</h1>
-    <RouterLink :to="{ name: 'map' }">MAP</RouterLink>
-  
-    <div>
       <RouterView />
-    </div>
   </div>
 
 </template>
--- front/bank-snap/src/router/index.js
+++ front/bank-snap/src/router/index.js
@@ -1,22 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MapView from '@/views/MapView.vue'
+import MainView from '@/views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'main',
+      component: MainView
+    },
+    {
+      path: '/interstrate',
+      name: 'interestrate',
+      component: () => import('../views/InterestRatesView.vue')
+    },
+    {
+      path: '/calculator',
+      name: 'exchangecalculator',
+      component: () => import('../views/ExchangeCalculatorView.vue')
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: () => import('../views/CommunityView.vue')
+    },
+    {
       path: '/map',
       name: 'map',
-      component: MapView
+      component: () => import('../views/MapView.vue')
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
