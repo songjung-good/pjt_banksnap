0df4d76fc1d0ae6c2da44e871e6afa28b902dd13
YH <139380475+songjung-good@users.noreply.github.com>
2023-11-16 15:13:19 +0900

    내가 뭘했더라?

--- front/bank-snap/src/main.js
+++ front/bank-snap/src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import BootstrapVue3 from 'bootstrap-vue-3'
 
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -14,7 +11,6 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 // app.use(createPinia())
-app.use(BootstrapVue3)
 app.use(pinia)
 app.use(router)
 
