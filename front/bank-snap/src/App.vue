97891a9013118cd8080aae8b47b93947e1d49aca
YH <139380475+songjung-good@users.noreply.github.com>
2023-11-20 10:18:15 +0900

    로그인 모양 생김

--- front/bank-snap/src/App.vue
+++ front/bank-snap/src/App.vue
@@ -45,42 +45,6 @@
   <body class="center">
     <RouterView :key="$route.fullPath"/>  
   </body>
-  <!-- <footer class="center">
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-    <button class="btn btn-primary py-2 dropdown-toggle d-flex align-items-center"
-      :id="bd-theme"
-      type="button"
-      aria-expanded="false"
-      data-bs-toggle="dropdown"
-      aria-label="Toggle theme (auto)">
-      <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use :href="themeIconActive()"></use></svg>
-      <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-    </button>
-    <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-      <li>
-        <button type="button" class="dropdown-item d-flex align-items-center" @click="toggleTheme('light')" aria-pressed="false">
-          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use :href="themeIconInactive()"></use></svg>
-          Light
-          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-        </button>
-      </li>
-      <li>
-        <button type="button" class="dropdown-item d-flex align-items-center" @click="toggleTheme('dark')" aria-pressed="false">
-          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use :href="themeIconInactive()"></use></svg>
-          Dark
-          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-        </button>
-      </li>
-      <li>
-        <button type="button" class="dropdown-item d-flex align-items-center active" @click="toggleTheme('auto')" aria-pressed="true">
-          <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use :href="themeIconActive()"></use></svg>
-          Auto
-          <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-        </button>
-      </li>
-    </ul>
-  </div>
-  </footer> -->
 </template>
 
 <script setup>
@@ -106,41 +70,6 @@ const closeNavbar = () => {
   isNavbarOpen.value = false;
 }
 
-const activeTheme = ref('auto');
-
-const toggleTheme = (theme) => {
-  activeTheme.value = theme;
-};
-
-const themeClasses = () => {
-  if (activeTheme.value === 'auto') {
-    return 'theme-auto';
-  } else if (activeTheme.value === 'light') {
-    return 'theme-light';
-  } else {
-    return 'theme-dark';
-  }
-};
-
-const themeIconActive = () => {
-  if (activeTheme.value === 'auto') {
-    return 'circle-half';
-  } else if (activeTheme.value === 'light') {
-    return 'sun-fill';
-  } else {
-    return 'moon-stars-fill';
-  }
-};
-
-const themeIconInactive = () => {
-  if (activeTheme.value === 'auto') {
-    return 'sun-fill';
-  } else if (activeTheme.value === 'light') {
-    return 'moon-stars-fill';
-  } else {
-    return 'circle-half';
-  }
-};
 </script>
 
 
--- front/bank-snap/src/views/LoginView.vue
+++ front/bank-snap/src/views/LoginView.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <p>아이디 : <input type="text" v-model.trim="username"></p>
-      <p>비밀번호 : <input type="password" v-model.trim="password"></p>
-      <input type="submit">
-    </form>
+  <div class="text-center">
+    <main class="form-signin">
+      <form @submit.prevent="login">
+        <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input type="text" v-model.trim="username" class="form-control" id="floatingInput" placeholder="ID">
+          <label for="floatingInput">ID</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" v-model.trim="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <div class="checkbox mb-3">
+          <label>
+            <input v-model="rememberMe" type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">Welcome! BankSnap</p>
+      </form>
+    </main>
   </div>
 </template>     
     
