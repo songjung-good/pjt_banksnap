cf7454d0a387b9f9b73831fd615b84f7a935a676
YH <139380475+songjung-good@users.noreply.github.com>
2023-11-21 18:02:34 +0900

    123123123

--- front/bank-snap/src/main.js
+++ front/bank-snap/src/main.js
@@ -4,6 +4,9 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+// import bootstrap5 from 'Bootstrap5'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -13,5 +16,6 @@ pinia.use(piniaPluginPersistedstate)
 // app.use(createPinia())
 app.use(pinia)
 app.use(router)
+// app.use(bootstrap5)
 
 app.mount('#app')
