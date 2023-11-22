9cde0c71ebd233a5c394db824c0a045b5f8ff8eb
지민 <81950388+jjm6604@users.noreply.github.com>
2023-11-22 12:29:22 +0900

    price_update

--- front/bank-snap/src/components/Price.vue
+++ front/bank-snap/src/components/Price.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <PriceItem 
-    v-for="item in items"
-    :key="item.name"
-    :item="item"
-    />
+  <div class="p-3">
+
+    
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <div class="nav-link" :class="{ 'active': activeTab === 'gold' }" @click="itemClick('gold', 0)">금</div>
+        </li>
+        <li class="nav-item">
+          <div class="nav-link" :class="{ 'active': activeTab === 'silver' }" @click="itemClick('silver', 1)">은</div>
+        </li>
+        <li class="nav-item">
+          <div class="nav-link" :class="{ 'active': activeTab === 'oil' }" @click="itemClick('oil', 2)">가솔린</div>
+        </li>
+        <li class="nav-item">
+          <div class="nav-link" :class="{ 'active': activeTab === 'gas' }" @click="itemClick('gas', 3)">천연가스</div>
+        </li>
+
+        <li class="nav-item">
+          <div class="nav-link" :class="{ 'active': activeTab === 'coffee' }" @click="itemClick('coffee', 4)">커피</div>
+        </li>
+
+      </ul>
+      <div v-if="item">
+        <PriceItem :item="item" />
+      </div>
   </div>
 </template>
 
@@ -16,21 +35,29 @@ import PriceItem from '@/components/PriceItem.vue'
 
 const store = useIndexStore()
 const items = ref([])
+const item = ref(null)
+const activeTab = ref('gold')
 onMounted(() => {
   axios({
   method: 'get',
   url: `${store.url}/bank/price/`,
-  // headers: {
+  // headers: { 
   //   Authorization: `Token ${store.token}`
   //   }
   })
     .then((res) => {
       items.value = res.data
+      item.value = items.value[0]
     })
     .catch((err) => {
       console.log(err)
     })
 })
+
+const itemClick = function(itemName, n){
+  activeTab.value = itemName
+  item.value = items.value[n]
+}
 </script>
 
 <style scoped>
--- front/bank-snap/src/components/PriceItem.vue
+++ front/bank-snap/src/components/PriceItem.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    <h3>실시간 {{ item.name }} 시세</h3>
-    <h5>{{ item.price }}</h5>
-    <p>{{ item.before_price }}</p>
-    <img :src="item.graph" alt="">
-    <hr>
+  <div class="card p-5">
+    <div class="card-body d-flex flex-wrap">
+      <div class="p-2 flex-grow-1">
+
+        <h3>실시간 {{ item.name }} 시세</h3>
+        <h5>{{ item.price }}</h5>
+        <p>{{ item.before_price }}</p>
+      </div>
+      <div class="p-2 flex-fill">
+
+        <img :src="item.graph" alt="graph">
+      </div>
+      
+    </div>
   </div>
 </template>
 
