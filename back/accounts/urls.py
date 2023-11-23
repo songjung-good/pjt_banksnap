0ac958c0f3d513532e1b078956076a4b7d4ef004
YH <139380475+songjung-good@users.noreply.github.com>
2023-11-23 11:36:22 +0900

    init

--- back/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/<str:username>/', views.profile),
-    path('propensity/', views.propensity),
-    path('test/', views.test),
-]
--- back/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('', views.article_list),
-  path('<int:article_pk>/', views.article_detail),
-  path('comments/<int:article_pk>/', views.comment),
-  path('comment/delete/<int:comment_pk>/', views.comment_delete)
-]
